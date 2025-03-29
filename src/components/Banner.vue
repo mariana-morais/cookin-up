cbec15bf6cffd27e6f7b24160208f01098bd5ee4
Mariana Morais <moraismariana8@gmail.com>
2025-03-29 20:34:05 -0300

    Add files

---
+++ src/components/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+    <main class="hero">
+        <div class="presentation">
+        <img src="../assets/images/logo.svg" alt="Logo of Cookin' Up" class="logo">
+
+        <p class="header-lg phrase-header">
+            <span class="green-text">Um banquete de ideias para</span>
+            despertar o chef que há em você!
+        </p>
+
+        <p class="subtitle-lg">
+            Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+        </p>
+        </div>
+
+        <img src="../assets/images/foto-banner.png" alt="Imagem de uma mulher cozinhando cm uma bacia de vidro nas mãos" class="hero-photo">
+  </main>
+</template>
+
+<style scoped lang="css">
+.hero {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.phrase-header {
+  margin-bottom: 2rem;
+}
+
+.green-text {
+  color: var(--medium-green, #3D6D4A);
+}
+
+.hero-photo {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .hero {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .hero {
+    padding: 4rem 1.5rem;
+  }
+
+  .hero-photo {
+    width: min(100%, 21.25rem);
+  }
+}
+</style>
---
+++ src/components/SelectIngredients.vue
@@ -0,0 +1,69 @@
+<script lang="ts">
+import { getCategories } from '@/http/index';
+
+export default {
+    data() {
+        return {
+            categories: getCategories()
+        }
+    }
+}
+</script>
+
+<template>
+    <setion class="select-ingredients">
+        <h1 class="header ingredients-title">Ingredientes</h1>
+
+
+        <p class="paragraph-lg instructions">
+            Selecione abaixo os ingredientes que você quer usar nesta receita:
+        </p>
+
+        <ul class="categories">
+            <li v-for="category in categories" :key="category.name">
+                {{ category.name }}
+            </li>
+        </ul>
+
+        <p class="paragraph hint">
+            *Atenção: consideramos que você tem em casa sal, pimenta e água.
+        </p>
+    </setion>
+</template>
+
+<style scoped>
+.select-ingredients {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.ingredients-title {
+  color: var(--medium-green, #3D6D4A);
+  display: block;
+  margin-bottom: 1.5rem;
+}
+
+.instructions {
+  margin-bottom: 2rem;
+}
+
+.categories {
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
+  flex-wrap: wrap;
+}
+
+.hint {
+  align-self: flex-start;
+  margin-bottom: 3.5rem;
+}
+
+@media only screen and (max-width: 767px) {
+  .hint {
+    margin-bottom: 2.5rem;
+  }
+}
+</style>
